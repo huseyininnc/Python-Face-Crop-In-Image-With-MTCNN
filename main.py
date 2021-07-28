61a9f2351858be8d7348270779508e5d30dd47c7
Hüseyin İnanç <huseyin.innc3304@gmail.com>
2021-07-28 03:25:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from utils import *
+from configparser import ConfigParser
+import argparse
+
+config_object = ConfigParser()
+config_object.read("config.conf")
+
+crop_mode = config_object["PATH"]["crop_mode"]
+faces_folder_path = config_object["PATH"]["faces_folder_path"]
+faces_image_path = config_object["PATH"]["faces_image_path"]
+cam_id = config_object["PATH"]["cam_id"]
+sleep_time = config_object["PATH"]["sleep_time"]
+multi_images_folder_path = config_object["PATH"]["multi_images_folder_path"]
+
+def main(args):
+    if args.mode == "cii":
+        face_crop_in_image(args.fip, args.ffp)
+
+    elif args.mode == "cimi":
+        face_crop_in_multi_images(args.mifp, args.ffp)
+    
+    elif args.mode == "civ":
+        face_crop_in_video(args.ci, args.ffp, args.st)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Face crop in image')
+    parser.add_argument('--mode', default=crop_mode, type=str, help='crop in image: cii, crop in multi images: cimi, crop in video: civ')
+    parser.add_argument('--ffp', default=faces_folder_path, type=str, help='faces folder path')
+    parser.add_argument('--fip', default=faces_image_path, type=str, help='face imges path')
+    parser.add_argument('--st', default=sleep_time, type=str, help='sleep time')
+    parser.add_argument('--mifp', default=multi_images_folder_path, type=str, help='multi face images path')
+    parser.add_argument('--ci', default=cam_id, type=str, help='camera id')
+    args = parser.parse_args()
+    main(args)
---
+++ utils.py
@@ -0,0 +1,55 @@
+import cv2, os, time
+from numpy import asarray
+from PIL import Image
+from mtcnn.mtcnn import MTCNN
+
+detector = MTCNN()
+
+def crop(img, results,face):
+    x1, y1, width, height = results[face]["box"]
+    x2, y2 = x1+width, y1+height
+    cropped_face = img[y1:y2, x1:x2]
+    face_image = Image.fromarray(cropped_face)
+    face_image = face_image.resize((224,224))
+    face_array = asarray(face_image)
+    return face_array
+
+def face_crop_in_image(faces_image_path, faces_folder_path):
+    img = cv2.imread(faces_image_path)
+    results = detector.detect_faces(img)
+    print("[PLEASE WAIT]")
+    start_time = time.time()
+    for i in range(len(results[0:])):
+        face_array = crop(img, results, i)
+        cv2.imwrite(f"{faces_folder_path}/{i}.jpg", face_array)
+    print(f"[FINISHED] [TIME: {time.time() - start_time :.2f} seconds]")
+
+def face_crop_in_multi_images(multi_images_folder_path, faces_folder_path):
+    all_img = os.listdir(f'{multi_images_folder_path}/.')
+    cropped_face_number = 0
+    print("[PLEASE WAIT]")
+    start_time = time.time()
+    for i in range(len(all_img)):
+        img = cv2.imread(f'{multi_images_folder_path}/{all_img[i]}')
+        results = detector.detect_faces(img)
+        for j in range(len(results[0:])):
+            face_array = crop(img, results, j)
+            cv2.imwrite(f"{faces_folder_path}/{cropped_face_number}.jpg", face_array)
+            cropped_face_number += 1
+    print(f"[FINISHED] [TIME: {time.time() - start_time :.2f} seconds]")
+
+
+def face_crop_in_video(cam_id, faces_folder_path, sleep_time):
+    video = cv2.VideoCapture(int(cam_id))
+    while True:
+        ret, img = video.read()
+        results = detector.detect_faces(img)
+        print("[PLEASE WAIT]")
+        start_time = time.time()
+        for i in range(len(results[0:])):
+            face_array = crop(img, results, i)
+            cv2.imwrite(f"{faces_folder_path}/{i}.jpg", face_array)
+            time.sleep(int(sleep_time))
+        video.release()
+        print(f"[FINISHED] [TIME: {time.time() - start_time :.2f} seconds]")
+
